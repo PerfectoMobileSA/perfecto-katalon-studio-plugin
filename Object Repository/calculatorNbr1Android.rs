170f498f497624b2d612c3062de92f89b4ac433d
Veronika Kramarenko <vkramarenko@perforce.com>
2022-10-17 14:39:17 +0300

    uploading updates for perfecto plugin 2022

---
+++ Object Repository/calculatorNbr1Android.rs
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>calculatorNbr1Android</name>
+   <tag></tag>
+   <elementGuidId>664fe77b-272b-41a2-876c-7ccd741e9a5f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@resource-id = 'com.sec.android.app.popupcalculator:id/calc_keypad_btn_01']</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@resource-id=&quot;com.sec.android.app.popupcalculator:id/calc_keypad_btn_01&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>resource-id</name>
+      <type>Main</type>
+      <value>com.sec.android.app.popupcalculator:id/calc_keypad_btn_01</value>
+      <webElementGuid>ccd4b48b-fd47-42dc-b44d-49dc08430313</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
--- Test Suites/iOS_Web.ts
+++ Test Suites/iOS_Web.ts
@@ -16,5 +16,6 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/SampleWeb</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
